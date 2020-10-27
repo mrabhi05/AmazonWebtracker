17a4adcec02ee2374d6b382f185da826a15503ab
Abhishek Nair <38916532+mrabhi05@users.noreply.github.com>
2020-10-27 15:38:06 +0530

    Completed the Tracker

--- tracker_config.py
+++ tracker_config.py
@@ -1,25 +1,29 @@
+  
 from selenium import webdriver
 
-DIRECTORY = 'reports',
-NAME = 'ps4',
-#CURRENCY = '₹',
-MIN_PRICE = '100',
-MAX_PRICE = '1000',
+DIRECTORY = 'reports'
+NAME = 'realme'
+CURRENCY = '₹'
+MIN_PRICE = '2000'
+MAX_PRICE = '50000'
 FILTERS = {
     'min': MIN_PRICE,
     'max': MAX_PRICE
 }
+BASE_URL = "http://www.amazon.in/"
 
-BASE_URL = "https://www.amazon.in"
+def get_chrome_web_driver(options):
+    return webdriver.Chrome("./chromedriver", chrome_options=options)
 
-def get_chrome_web_driver(option):
-    return webdriver.Chrome('./chromedriver', chrome_options=option)
-
-def get_chrome_web_options():
+def get_web_driver_options():
     return webdriver.ChromeOptions()
 
-def set_ignore_certificate_error(option):
-    option.add_argument('--ignore-certificate-errors')
+def set_ignore_certificate_error(options):
+    options.add_argument('--ignore-certificate-errors')
 
-def set_browser_as_incognito(option):
-    option.add_argument('--incognito')
+
+def set_browser_as_incognito(options):
+    options.add_argument('--incognito')
+
+def set_automation_as_head_less(options):
+    options.add_argument('--headless')
